4ecc1579fe01a3a592b892a38c285d965561ce53
Valerii <valeravivaz@gmail.com>
2022-01-07 15:21:01 +0200

    Add uncontroled input typing example

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,19 +1,21 @@
-import React, {FC, ReactEventHandler, useState} from 'react'
+import React, {FC, ReactEventHandler, useRef, useState} from 'react'
 
 export const Events: FC = () => {
   const [value, setValue] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
   }
 
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) =>{
-    alert(value)
+    alert(`uncontroled: ${inputRef.current?.value || "-empty-"}, controled: ${value || "-empty-"}`)
   }
 
   return (
     <div style={{paddingTop: "20px", paddingBottom: "30px"}}>
-      <input value={value} onChange={changeHandler} type="text" />
+      <input ref={inputRef} type="text" placeholder="uncontroled input"/>
+      <input value={value} onChange={changeHandler} type="text" placeholder="controled input" />
       <button onClick={clickHandler}>alert!</button>
     </div>
   )
