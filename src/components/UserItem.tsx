3678840dd8309d1bd754aa8de8163678ad62e3ce
Valerii <valeravivaz@gmail.com>
2022-01-07 23:01:11 +0200

    Refactor

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -3,11 +3,12 @@ import { IUser } from "../types/types";
 
 interface UserItemProps {
   user: IUser;
+  onClick: (user: IUser) => void;
 }
 
-export const UserItem: FC<UserItemProps> = ({ user }) => {
+export const UserItem: FC<UserItemProps> = ({ user, onClick }) => {
   return (
-    <div style={{ padding: 15, border: "1px solid green" }}>
+    <div onClick={()=>onClick(user)} style={{ padding: 15, border: "1px solid green" }}>
       id:{user.id} name:{user.name} address:{user.address.zipcode}{" "}
       {user.address.city} {user.address.street}
     </div>
